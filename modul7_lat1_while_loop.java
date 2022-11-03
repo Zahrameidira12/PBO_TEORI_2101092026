cfb4b7664876e1cf1feb0971b8306b9b4e6fb82e
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-03 21:24:52 +0700

    Create modul7_lat1_while_loop.java

---
+++ modul7_lat1_while_loop.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori_modul7;
+
+/**
+ *
+ * @author HP
+ */
+public class modul7_lat1_while_loop {
+    {
+        String[] hari = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        int i=0;
+        while(i<hari.length)
+            {
+                System.out.println(hari[i]);
+               i++;
+            }
+    }
+}
