1e7904036f5ee335d91cef4393ffbd46bc487eba
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-03 21:24:38 +0700

    Create modul7_lat1_for_loop.java

---
+++ modul7_lat1_for_loop.java
@@ -0,0 +1,23 @@
+package pbo_teori_modul7;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class modul7_lat1_for_loop {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+                ;
+            }
+    }
+}
